b224966c213059978664f24cff54d81f9acf8288
lucalorenzon <X86@LAPTOP-HOME>
2016-07-29 15:46:33 +0200

    aggiunti altri enum + creati metodi getRegister e getByteRegister

--- NRF24.cpp
+++ NRF24.cpp
@@ -33,7 +33,7 @@ void NRF24::init(uint8_t cePin, uint8_t csnPin) {
     this->bcm2835->beginSPI(this->csnPin);
 }
 
-uint8_t NRF24::getStatus(){
+uint8_t NRF24::getStatusRegister(){
     uint8_t statusValue=0;
     if(this->bcm2835!=NULL){
        statusValue=this->bcm2835->transfer(NRF24.NOP_COMMAND); //NOP=0xFF
@@ -41,6 +41,50 @@ uint8_t NRF24::getStatus(){
        this->lstStatusUpdateTime=time(NULL);
     }
     return statusValue;
+}
+
+uint8_t NRF24::getByteRegister(NRF24_REGISTER register){
+    uint8_t statusValue=0;
+    if(this->bcm2835!=NULL){
+        uint8_t *txBuff = new uint8_t[2];
+        uint8_t *rxBuff = new uint8_t[2];
+        txBuff[0]= OP_R_REGISTER | static_cast<uint8_t>(register);
+        txBuff[1]=NOP_COMMAND;
+        this->bcm2835->transferb(rxBuff, txBuff, 2);
+        statusValue=rxBuff[0];
+        this->lastStatus=statusValue;
+        this->lstStatusUpdateTime=time(NULL);
+        uint8_t returnValue=rxBuff[1];
+        delete txBuff;
+        delete rxBuff;
+        return returnValue;
+    }
+}
+
+void NRF24::getRegister(NRF24_REGISTER register, uint8_t *value, uint32_t lenght){
+    uint8_t statusValue=0;
+    if(this->bcm2835!=NULL){
+        uint8_t *txBuff = new uint8_t[lenght+1];
+        uint8_t *rxBuff = new uint8_t[lenght+1];
+        txBuff[0]= OP_R_REGISTER | static_cast<uint8_t>(register);
+        for (int i = 1; i < lenght+1; i++) {
+            txBuff[i]=NOP_COMMAND;
+        }
+
+        this->bcm2835->transferb(rxBuff, txBuff, lenght+1);
+        statusValue=rxBuff[0];
+        this->lastStatus=statusValue;
+        this->lstStatusUpdateTime=time(NULL);
+        for (int i = 1; i < lenght+1; i++) {
+            value[i-1] = rxBuff[i];
+        }
+        delete txBuff;
+        delete rxBuff;
+    }
+}
+
+void NRF24::powerUp(){
+    
 }
 
 NRF24::~NRF24() {
--- NRF24.h
+++ NRF24.h
@@ -24,16 +24,32 @@ public:
     NRF24(uint8_t cePin, uint8_t csnPin);
     NRF24(const NRF24& orig);
     virtual ~NRF24();
-    uint8_t NRF24::getStatus();
+    uint8_t NRF24::getStatusRegister();
+    void powerUp();
+    uint8_t getByteRegister(NRF24_REGISTER register);
+    void getRegister(NRF24_REGISTER register, uint8_t *value, uint32_t lenght);
+    
 private:
-    static enum COMMAND{NOP_COMMAND=0xFF};
-    enum STATUS {PWR_DOWN, STARTUP, STANDBY_1, STANDBY_2, RX_SETTING, RX_MODE, TX_SETTING, TX_MODE} 
+    static enum NRF24_COMMAND{NOP_COMMAND=0xFF, OP_R_REGISTER=0b00000000, OP_W_REGISTER=0b00100000, 
+                        OP_R_RX_PAYLOAD=0b01100001, OP_W_TX_PAYLOAD=0b10100000, 
+                        OP_FLUSH_TX=0b11100001, OP_FLUSH_RX=0b11100010,
+                        OP_REUSE_TX_PL=0b11100011, OP_R_RX_PL_WID=0b01100000,
+                        OP_W_ACK_PAYLOAD=0b10101000, OP_W_TX_PAYLOAD_NOACK=0b10110000};
+    enum NRF24_STATUS {PWR_DOWN, STARTUP, STANDBY_1, STANDBY_2, RX_SETTING, RX_MODE, TX_SETTING, TX_MODE}; 
+    enum NRF24_REGISTER {CONFIG=0x00, EN_AA=0x01, EN_RXADDR=0x02, SETUP_AW=0x03, SETUP_RETR=0x04, RF_CH=0x05,
+                         RF_SETUP=0x06, STATUS_REG=0x07, OBSERVE_TX=0x08, RPD=0x09, 
+                         RX_ADDR_P0=0x0A, RX_ADDR_P1=0x0B, RX_ADDR_P2=0x0C, RX_ADDR_P3=0x0D, RX_ADDR_P4=0x0E, RX_ADDR_P5=0x0F,
+                         TX_ADDR=0x10, 
+                         RX_PW_P0=0x11, RX_PW_P1=0x12, RX_PW_P2=0x13, RX_PW_P3=0x14, RX_PW_P4=0x15, RX_PW_P5=0x16,
+                         FIFO_STATUS=0x17, DYNPLD=0x1C, FEATURE=0x1D};
     uint8_t lastStatus;
     time_t lstStatusUpdateTime;
     BCM283X *bcm2835;
     uint8_t cePin;
     uint8_t csnPin;
     void init(uint8_t cePin, uint8_t csnPin);
+    
+    
 };
 
 #endif /* NRF24_H */
